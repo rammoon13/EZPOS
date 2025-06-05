f7c7d9c222c0af0b1bac0546b40bf9901ef2e474
rammoon13 <herreraroblesramon@gmail.com>
2025-06-05 09:36:20 +0200

    separacion por fechas (importaciones)

--- app/src/main/java/com/example/ezpos/fragments/PedidosFragment.java
+++ app/src/main/java/com/example/ezpos/fragments/PedidosFragment.java
@@ -32,6 +32,7 @@ import com.example.ezpos.IntroHelper;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
